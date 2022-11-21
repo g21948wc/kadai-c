7505f01f1587b8ae6af995b71caa1f76738a9a25
g21948wc <113568957+g21948wc@users.noreply.github.com>
2022-11-21 11:08:22 +0900

    Update tokei.js

--- tokei.js
+++ tokei.js
@@ -1,6 +1,6 @@
 setInterval(function(){
 	const date1 = document.querySelector("#date");
-	Const time = document.querySelector("#time");
+	const time = document.querySelector("#time");
 	let date = new Date();
 	let month = date.getMonth();
 	let day = date.getDate();
@@ -10,22 +10,23 @@ setInterval(function(){
 	let seconds = date.getSeconds();
 	let day_night = "AM";
 
-if(hours > 12){
-	day_night = "PM";
-	hours = hours - 12;
-}
+ if(hours > 12){
+	 day_night = "PM";
+	 hours = hours - 12;
+ }
 
-if(hours < 10){
-	hours = "0" + hours;
-}
+ if(hours < 10){
+	 hours = "0" + hours;
+ }
 
-if(minutes < 10){
-	minutes = "0" + minutes;
-}
+ if(minutes < 10){
+	 minutes = "0" + minutes;
+ }
 
-if(seconds < 10){
-	seconds = "0" + seconds;
-}
+ if(seconds < 10){
+	 seconds = "0" + seconds;
+ }
  
-date1.textContent = month + "/" + day + "(" + days[date.getDay()] + ")".
-time.textContent = hours + ":" + minutes + ":" + seconds + " " + day_night;
+ date1.textContent = month + "/" + day + "(" + days[date.getDay()] + ")".
+ time.textContent = hours + ":" + minutes + ":" + seconds + " " + day_night;
+});
